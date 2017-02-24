7fe2447fa909b3cda37c8e331b2300f329d028f4
Tim Getz <tgetz@arcweb.co>
2017-02-23 23:26:28 -0500

    Added sliding menu for mobile.  Included web animation pollyfill.

---
+++ src/app/mobile/mobile-sliding-panel/mobile-sliding-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'st-mobile-sliding-panel',
+  templateUrl: './mobile-sliding-panel.component.html',
+  styleUrls: ['./mobile-sliding-panel.component.css']
+})
+export class MobileSlidingPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
